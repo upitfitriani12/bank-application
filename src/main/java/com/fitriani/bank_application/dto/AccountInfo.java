d3a0148e775ae2281687456b4f9e76d637c8234f
upit <upit.upit@enigma.com>
2025-01-18 20:07:14 +0700

    [add] - dto and user repository

---
+++ src/main/java/com/fitriani/bank_application/dto/AccountInfo.java
@@ -0,0 +1,17 @@
+package com.fitriani.bank_application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountInfo {
+
+    private String accountName;
+    private String accountBalance;
+    private String accountNumber;
+}
---
+++ src/main/java/com/fitriani/bank_application/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.fitriani.bank_application.repository;
+
+import com.fitriani.bank_application.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
